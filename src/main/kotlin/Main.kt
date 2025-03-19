2489b1af56b1a3bd94737d81db5a544c6aa47c2c
oleg <ip.pan@bk.ru>
2025-03-20 00:31:57 +0400

    KS-5-8 create parser citation

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,15 @@
 package org.example
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+import org.jsoup.Jsoup
+import org.jsoup.select.Elements
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+fun main() {
+
+    val url = "https://mybook.ru/author/duglas-adams/avtostopom-po-galaktike-restoran-u-konca-vselennoj/citations/"
+    val doc = Jsoup.connect(url).get()
+    val quoteElements: Elements = doc.select("div.sc-2aegk7-2")
+
+    for (quote in quoteElements){
+        println("${quote.text()}\n")
     }
 }
